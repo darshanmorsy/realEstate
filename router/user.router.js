6bffa52c325a5184a69270089e1d2dfd2dd48767
darshanmorsy <darshanmorsy@gmail.com>
2023-07-13 12:21:07 +0530

    hh

--- router/user.router.js
+++ router/user.router.js
@@ -22,22 +22,7 @@ const {
   allproperty,
   rent,
   sell,
-  rent_flat,
-  rent_bungalow,
-  rent_shop,
-  rent_office,
-  rent_openplot,
-  rent_rowhouse,
-  rent_industrial,
-  rent_farm,
-  sell_flat,
-  sell_bungalow,
-  sell_shop,
-  sell_office,
-  sell_openplot,
-  sell_rowhouse,
-  sell_industrial,
-  sell_farm,
+  rent_buy_property,
   frontfilter,
   filterpost,
   deleteproperty,
@@ -71,23 +56,10 @@ router.post("/contact/:id", contact)
 router.get("/allproperty", allproperty)
 router.get("/rent", rent)
 router.get("/sell", sell)
-router.get("/rent_flat", rent_flat)
-router.get("/rent_office", rent_office)
-router.get("/rent_shop", rent_shop)
-router.get("/rent_banglow", rent_bungalow) 
-router.get("/rent_openplot", rent_openplot)
-router.get("/rent_rowhouse", rent_rowhouse)
-router.get("/rent_industrial", rent_industrial)
-router.get("/rent_farm", rent_farm)
+router.get("/rent_buy_property/:category/:house_etype",rent_buy_property )
 
-router.get("/sell_flat", sell_flat)
-router.get("/sell_office", sell_office)
-router.get("/sell_shop", sell_shop)
-router.get("/sell_banglow", sell_bungalow)
-router.get("/sell_openplot", sell_openplot)
-router.get("/sell_rowhouse", sell_rowhouse)
-router.get("/sell_industrial", sell_industrial)
-router.get("/sell_farm", sell_farm)
+
+
 
 router.get("/filter/:city/:category/:house_type/:lessrange/:greaterrange",frontfilter)
 router.get("/user_property", user_token, user_property)
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ require("dotenv").config()
 app.use(express.static(path.join(__dirname, "assets")))
 
 
+
 const flash = require("express-flash");
 
 app.use(flash());
