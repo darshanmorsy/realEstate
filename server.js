ce4ee998e0c5faab4e0c14baa7618df39fe53ae6
darshanmorsy <darshanmorsy@gmail.com>
2023-07-28 17:24:21 +0530

    hh

--- server.js
+++ server.js
@@ -11,23 +11,23 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 require("dotenv").config()
 app.use(express.static(path.join(__dirname, "assets")))
-app.use(express.static(path.join(__dirname, 'upload')));
+app.use(express.static(path.join(__dirname, 'upload')))
 
 
-const flash = require("express-flash");
+const flash = require("express-flash")
 
-app.use(flash());
+app.use(flash())
 
 app.use(session({
     secret: 'mysecret', // Replace with your own secret key
     resave: false,
     saveUninitialized:false            
-  }));
+  }))
   
 
 
 
-const db = "mongodb+srv://morsy:morsy@ds.6e7bjag.mongodb.net/realEstate";
+const db = "mongodb+srv://morsy:morsy@ds.6e7bjag.mongodb.net/realEstate"
 mongoose
   .connect(db, {
     // useCreateIndex: true,
@@ -36,29 +36,29 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err))
 
-const port = process.env.PORT || 3000;
-// require("./db/connection"); 
+const port = process.env.PORT || 3000
+// require("./db/connection") 
  
-app.use(cors());
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(logger("dev"));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(logger("dev"))
 
-const userRouter = require("./router/user.router");
-const adminRouter = require("./router/admin.router");
+const userRouter = require("./router/user.router")
+const adminRouter = require("./router/admin.router")
 
-app.use("/user", userRouter);
-app.use("/admin", adminRouter);
+app.use("/user", userRouter)
+app.use("/admin", adminRouter)
 app.get("/", (req, res) => {
   res.redirect("/user")
 })
 
 
 app.listen(port, () => {
-  console.log(`listing to the port ${port}`);
-});
+  console.log(`listing to the port ${port}`)
+})
  
